946ac70a25f4cb18ff4466c682ec50108f389580
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-10-26 15:35:35 +0200

    correct

--- src/app/Contactus/page.tsx
+++ src/app/Contactus/page.tsx
@@ -85,205 +85,223 @@ const Contactus = () => {
     <>
       {/* SEO metadata for the contact page */}
       <Head>
-      <link rel="icon" href="src/app/favicon.ico" /> 
+        <link rel="icon" href="src/app/favicon.ico" />
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
-    <div
-      className={` ${
-        theme === "dark" ? "bg-slate-700" : ""
-      } relative flex flex-col justify-center items-center`}
-    >
-      <div className="relative bg-[#1b396e] h-[400px] md:h-[450px] lg:h-[450px] w-full overflow-hidden">
-        <Image
-          src={image2}
-          alt="image 2"
-          layout="responsive"
-          width={100}
-          height={20}
-          className="w-full opacity-10"
-        />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">
-          <p className="font-merriweather text-center text-xl md:text-[70px] text-[#ffffff] font-bold">
-            {language === "en" ? "Get In Touch" : "Entrer en contact"}
-          </p>
-          <p className="font-roboto mt-6 md:mt-10 text-center text-[20px]">
-            {language === "en"
-              ? " Want to get in touch? we'd love to hear from you. Here is how you can reach us."
-              : "Vous souhaitez nous contacter ? nous aimerions avoir de vos nouvelles. Voici comment vous pouvez nous joindre."}
-          </p>
-        </div>
-      </div>
-
-      <div className="relative bg-gradient-to-b from-gray-200 via-gray-200/90 w-full flex flex-col md:flex-col lg:flex-row overflow-hidden">
-        <div className="p-5 md:p-10 flex justify-center items-center w-full md:w-full lg:w-1/2">
-          <iframe
-            className="shadow-xl w-full h-[300px] md:h-[400px] lg:h-[500px]"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.372925704017!2d-75.70215228469056!3d45.352704179101964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccebb0cb651c455%3A0x445c123d0cf4b689!2s19%20Grenfell%20Crescent%2C%20Ottawa%2C%20ON%2C%20Canada%20K2G%200G3!5e0!3m2!1sen!2sus!4v1697198948007!5m2!1sen!2sus"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+      <div
+        className={` ${
+          theme === "dark" ? "bg-slate-700" : ""
+        } relative flex flex-col justify-center items-center`}
+      >
+        <div className="relative bg-[#1b396e] h-[400px] md:h-[450px] lg:h-[450px] w-full overflow-hidden">
+          <Image
+            src={image2}
+            alt="image 2"
+            layout="responsive"
+            width={100}
+            height={20}
+            className="w-full opacity-10"
+          />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">
+            <p className="font-merriweather text-center text-xl md:text-[70px] text-[#ffffff] font-bold">
+              {language === "en" ? "Get In Touch" : "Entrer en contact"}
+            </p>
+            <p className="font-roboto mt-6 md:mt-10 text-center text-[20px]">
+              {language === "en"
+                ? " Want to get in touch? we'd love to hear from you. Here is how you can reach us."
+                : "Vous souhaitez nous contacter ? nous aimerions avoir de vos nouvelles. Voici comment vous pouvez nous joindre."}
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col p-10 md:p-5 md:mt-10 lg:mt-10 w-full md:w-full lg:w-1/2">
-          <h1 className="text-[1.7rem] text-[#1b396e] text-center font-normal font-merriweather pb-2">
-            {language == "en" ? " Connect With Us" : "Connectez-vous avec nous"}
-          </h1>
+        <div className="relative bg-gradient-to-b from-gray-200 via-gray-200/90 w-full flex flex-col md:flex-col lg:flex-row overflow-hidden">
+          <div className="p-5 md:p-10 flex justify-center items-center w-full md:w-full lg:w-1/2">
+            <iframe
+              className="shadow-xl w-full h-[300px] md:h-[400px] lg:h-[500px]"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.372925704017!2d-75.70215228469056!3d45.352704179101964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccebb0cb651c455%3A0x445c123d0cf4b689!2s19%20Grenfell%20Crescent%2C%20Ottawa%2C%20ON%2C%20Canada%20K2G%200G3!5e0!3m2!1sen!2sus!4v1697198948007!5m2!1sen!2sus"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
 
-          <div className="flex flex-col mt-3 items-center">
-            <div className="flex flex-row items-center">
-              <Image src={message} alt="message image" width={20} />
-              <a
-                href="mailto:info@cyberprogroup.com "
-                className="ml-2 text-[#1e1e1e] hover:underline font-thin"
-              >
-                info@cyberprogroup.com
-              </a>
-            </div>
-
-            <div className="flex flex-row items-center mt-2">
-              <Image src={telephone} alt="telephone image" width={20} />
-              <p className="ml-2 text-[#1e1e1e] font-thin">
-                +237691181569 / +237670-93-57-21
-              </p>
-            </div>
-            <div className="flex flex-row items-center mt-2">
-              <Image src={telephone} alt="telephone image" width={20} />
-              <p className="ml-2 text-[#1e1e1e] font-thin">+1 514-452-4048</p>
-            </div>
-            <div className="flex flex-row items-center mt-2">
-              <Image src={telephone} alt="telephone image" width={20} />
-              <p className="ml-2 text-[#1e1e1e] font-thin">+250795305033</p>
-            </div>
-
-            <h1 className="text-[1.5rem] text-[#1b396e] text-center font-normal font-merriweather pb-2 mt-10 mb-3">
-              {language == "en" ? "   Where to Find Us" : "Où nous trouver"}
+          <div className="flex flex-col p-10 md:p-5 md:mt-10 lg:mt-10 w-full md:w-full lg:w-1/2">
+            <h1 className="text-[1.7rem] text-[#1b396e] text-center font-normal font-merriweather pb-2">
+              {language == "en"
+                ? " Connect With Us"
+                : "Connectez-vous avec nous"}
             </h1>
 
-            <div className="flex flex-row items-center">
-              <FaLocationDot width={20} />
-              <p className="ml-2 mt-5 text-[#1e1e1e] font-thin text-center max-w-lg">
-                KN 4 Ave, Kigali, Rwanda
-              </p>
-            </div>
-            <div className="flex flex-row items-center">
-              <FaLocationDot width={20} />
-              <p className="ml-2 mt-5 text-[#1e1e1e] font-thin text-center max-w-lg">
-                19 Grenfell Crescent, Ottawa ON, Canada K2G0G3
-              </p>
-            </div>
-            <div className="flex flex-row items-center">
-              <FaLocationDot width={20} />
-              <p className="ml-2 mt-5 text-[#1e1e1e] font-thin text-center max-w-lg">
-                Checkpoint Molyko, Buea, Cameroon
-              </p>
+            <div className="flex flex-col mt-3 items-center">
+              <div className="flex flex-row items-center">
+                <Image src={message} alt="message image" width={20} />
+                <a
+                  href="mailto:info@cyberprogroup.com "
+                  className="ml-2 text-[#1e1e1e] hover:underline font-thin"
+                >
+                  info@cyberprogroup.com
+                </a>
+              </div>
+
+              <div className="flex flex-row items-center mt-2">
+                <Image src={telephone} alt="telephone image" width={20} />
+                <p className="ml-2 text-[#1e1e1e] font-thin">
+                  +237691181569 / +237670-93-57-21
+                </p>
+              </div>
+              <div className="flex flex-row items-center mt-2">
+                <Image src={telephone} alt="telephone image" width={20} />
+                <p className="ml-2 text-[#1e1e1e] font-thin">
+                  +1 (302) 513-5995
+                </p>
+              </div>
+              <div className="flex flex-row items-center mt-2">
+                <Image src={telephone} alt="telephone image" width={20} />
+                <p className="ml-2 text-[#1e1e1e] font-thin">+1 514-452-4048</p>
+              </div>
+              <div className="flex flex-row items-center mt-2">
+                <Image src={telephone} alt="telephone image" width={20} />
+                <p className="ml-2 text-[#1e1e1e] font-thin">+250795305033</p>
+              </div>
+
+              <h1 className="text-[1.5rem] text-[#1b396e] text-center font-normal font-merriweather pb-2 mt-10 mb-3">
+                {language == "en" ? "   Where to Find Us" : "Où nous trouver"}
+              </h1>
+
+              <div className="flex flex-row items-center">
+                <FaLocationDot width={20} />
+                <p className="ml-2 mt-5 text-[#1e1e1e] font-thin text-center max-w-lg">
+                  KN 4 Ave, Kigali, Rwanda
+                </p>
+              </div>
+              <div className="flex flex-row items-center">
+                <FaLocationDot width={20} />
+                <p className="ml-2 mt-5 text-[#1e1e1e] font-thin text-center max-w-lg">
+                  19 Grenfell Crescent, Ottawa ON, Canada K2G0G3
+                </p>
+              </div>
+              <div className="flex flex-row items-center">
+                <FaLocationDot width={20} />
+                <p className="ml-2 mt-5 text-[#1e1e1e] font-thin text-center max-w-lg">
+                  USA
+                </p>
+              </div>
+              <div className="flex flex-row items-center">
+                <FaLocationDot width={20} />
+                <p className="ml-2 mt-5 text-[#1e1e1e] font-thin text-center max-w-lg">
+                  Checkpoint Molyko, Buea, Cameroon
+                </p>
+              </div>
             </div>
           </div>
         </div>
+
+        <div className="flex flex-col items-center justify-center rounded-xl mt-10">
+          <div className="flex flex-col items-center mb-16 md:mb-24">
+            <h1 className="font-merriweather text-[20px] md:text-[30px] lg:text-[30px] text-[#1b396e] font-bold">
+              {language === "en"
+                ? "Have a Question? Contact Us."
+                : "Vous avez une question ? Contactez-nous."}
+            </h1>
+            <div className="bg-[#f39c12] w-32 h-2 md:w-48 lg:w-48 md:h-2 lg:h-2 rounded-2xl mt-2 md:mt-3" />
+          </div>
+
+          <div className="flex flex-col m-2">
+            <input
+              required
+              type="text"
+              name="name"
+              placeholder={
+                language === "en"
+                  ? "Please Enter Your Name"
+                  : "Veuillez entrer votre nom"
+              }
+              className={`bg-[#ffff] w-[350px] md:w-[700px] h-14 text-[#1e1e1e] border ${
+                errors.name ? "border-red-500" : "border-[#363636]"
+              } outline-[#1b396e] text-[20px] font-roboto text-start pl-3 rounded-[5px]`}
+              value={input.name}
+              onChange={handleOnChange}
+            />
+            {errors.name && <p className="text-red-500">{errors.name}</p>}
+          </div>
+
+          <div className="flex flex-col m-2">
+            <input
+              required
+              type="email"
+              name="email"
+              placeholder={
+                language === "en"
+                  ? "Please Enter Your Email"
+                  : "Veuillez entrer votre email"
+              }
+              className={`bg-[#ffff] w-[350px] md:w-[700px] h-14 text-[#1e1e1e] border ${
+                errors.email ? "border-red-500" : "border-[#363636]"
+              } outline-[#1b396e] text-[20px] font-roboto text-start pl-3 rounded-[5px]`}
+              value={input.email}
+              onChange={handleOnChange}
+            />
+            {errors.email && <p className="text-red-500">{errors.email}</p>}
+          </div>
+
+          <div className="flex flex-col m-2">
+            <input
+              required
+              type="text"
+              name="location"
+              placeholder={
+                language === "en"
+                  ? "Enter Location"
+                  : "Veuillez entrer votre Location"
+              }
+              className={`bg-[#ffff] w-[350px] md:w-[700px] h-14 text-[#1e1e1e] border ${
+                errors.location ? "border-red-500" : "border-[#363636]"
+              } outline-[#1b396e] text-[20px] font-roboto text-start pl-3 rounded-[5px]`}
+              value={input.location}
+              onChange={handleOnChange}
+            />
+            {errors.location && (
+              <p className="text-red-500">{errors.location}</p>
+            )}
+          </div>
+
+          <div className="flex flex-col m-2">
+            <textarea
+              required
+              name="message"
+              placeholder={
+                language === "en"
+                  ? "Enter Your Message"
+                  : "Entrez votre message"
+              }
+              className={`bg-[#ffff] w-[350px] md:w-[700px] min-h-52 md:min-h-72 text-[#1e1e1e] border ${
+                errors.message ? "border-red-500" : "border-[#363636]"
+              } outline-[#1b396e] text-[20px] font-roboto text-start pl-3 rounded-[5px]`}
+              value={input.message}
+              onChange={handleOnChange}
+            />
+            {errors.message && <p className="text-red-500">{errors.message}</p>}
+          </div>
+
+          <div className="flex flex-col m-2">
+            <button
+              onClick={() => onSubmit()}
+              className="bg-[#1b396e] w-[350px] md:w-[700px] h-10 text-center border-none outline-none text-[20px] font-roboto pl-3 rounded-[5px] text-[#ffff] font-bold hover:bg-blue-800 transition-all duration-300 ease-in-out"
+            >
+              {isLoading
+                ? language === "en"
+                  ? "Sending.."
+                  : "Envoyer..."
+                : language === "en"
+                ? "Send Message"
+                : "Envoyer un message"}
+            </button>
+          </div>
+        </div>
       </div>
-
-      <div className="flex flex-col items-center justify-center rounded-xl mt-10">
-        <div className="flex flex-col items-center mb-16 md:mb-24">
-          <h1 className="font-merriweather text-[20px] md:text-[30px] lg:text-[30px] text-[#1b396e] font-bold">
-            {language === "en"
-              ? "Have a Question? Contact Us."
-              : "Vous avez une question ? Contactez-nous."}
-          </h1>
-          <div className="bg-[#f39c12] w-32 h-2 md:w-48 lg:w-48 md:h-2 lg:h-2 rounded-2xl mt-2 md:mt-3" />
-        </div>
-
-        <div className="flex flex-col m-2">
-          <input
-            required
-            type="text"
-            name="name"
-            placeholder={
-              language === "en"
-                ? "Please Enter Your Name"
-                : "Veuillez entrer votre nom"
-            }
-            className={`bg-[#ffff] w-[350px] md:w-[700px] h-14 text-[#1e1e1e] border ${
-              errors.name ? "border-red-500" : "border-[#363636]"
-            } outline-[#1b396e] text-[20px] font-roboto text-start pl-3 rounded-[5px]`}
-            value={input.name}
-            onChange={handleOnChange}
-          />
-          {errors.name && <p className="text-red-500">{errors.name}</p>}
-        </div>
-
-        <div className="flex flex-col m-2">
-          <input
-            required
-            type="email"
-            name="email"
-            placeholder={
-              language === "en"
-                ? "Please Enter Your Email"
-                : "Veuillez entrer votre email"
-            }
-            className={`bg-[#ffff] w-[350px] md:w-[700px] h-14 text-[#1e1e1e] border ${
-              errors.email ? "border-red-500" : "border-[#363636]"
-            } outline-[#1b396e] text-[20px] font-roboto text-start pl-3 rounded-[5px]`}
-            value={input.email}
-            onChange={handleOnChange}
-          />
-          {errors.email && <p className="text-red-500">{errors.email}</p>}
-        </div>
-
-        <div className="flex flex-col m-2">
-          <input
-            required
-            type="text"
-            name="location"
-            placeholder={
-              language === "en"
-                ? "Enter Location"
-                : "Veuillez entrer votre Location"
-            }
-            className={`bg-[#ffff] w-[350px] md:w-[700px] h-14 text-[#1e1e1e] border ${
-              errors.location ? "border-red-500" : "border-[#363636]"
-            } outline-[#1b396e] text-[20px] font-roboto text-start pl-3 rounded-[5px]`}
-            value={input.location}
-            onChange={handleOnChange}
-          />
-          {errors.location && <p className="text-red-500">{errors.location}</p>}
-        </div>
-
-        <div className="flex flex-col m-2">
-          <textarea
-            required
-            name="message"
-            placeholder={
-              language === "en" ? "Enter Your Message" : "Entrez votre message"
-            }
-            className={`bg-[#ffff] w-[350px] md:w-[700px] min-h-52 md:min-h-72 text-[#1e1e1e] border ${
-              errors.message ? "border-red-500" : "border-[#363636]"
-            } outline-[#1b396e] text-[20px] font-roboto text-start pl-3 rounded-[5px]`}
-            value={input.message}
-            onChange={handleOnChange}
-          />
-          {errors.message && <p className="text-red-500">{errors.message}</p>}
-        </div>
-
-        <div className="flex flex-col m-2">
-          <button
-            onClick={() => onSubmit()}
-            className="bg-[#1b396e] w-[350px] md:w-[700px] h-10 text-center border-none outline-none text-[20px] font-roboto pl-3 rounded-[5px] text-[#ffff] font-bold hover:bg-blue-800 transition-all duration-300 ease-in-out"
-          >
-            {isLoading
-              ? language === "en"
-                ? "Sending.."
-                : "Envoyer..."
-              : language === "en"
-              ? "Send Message"
-              : "Envoyer un message"}
-          </button>
-        </div>
-      </div>
-    </div>
     </>
   );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,209 @@
-.footer-container {
-  background: url('/image/footer.jpg') no-repeat center center / cover;
-}
+"use client";
+import { SiSpringsecurity } from "react-icons/si";
+import Link from "next/link";
+import {
+  Contacts,
+  Locations,
+  NavigationItemsRoutes,
+} from "./HeaderAndFooter/constant";
+import {
+  FaEnvelope,
+  FaTiktok,
+  FaYoutube,
+  FaLinkedin,
+  FaInstagram,
+  FaFacebookF,
+} from "react-icons/fa";
+import { useAppContext } from "@/context/appContext";
+import { useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "@/context/api";
+import { toast } from "react-toastify";
+import { FaXTwitter } from "react-icons/fa6";
 
-.footer-section {
-  flex: 1;
-  margin: 1rem;
-}
+const Footer = () => {
+  const { language } = useAppContext();
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
-.divider {
-  width: 50px;
-  background: blue;
-  height: 2px;
-  margin: 0.5rem 0;
-}
+  const handleSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post(`${BASE_URL}/user/subscribe`, {
+        email,
+      });
+      toast.success(response.data.message);
+    } catch (error) {
+      toast.error("Failed! Try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-.social-media {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+  return (
+    <div
+      className="bg-cover bg-center p-5"
+      style={{ backgroundImage: "url('/image/footer.jpg')" }}
+    >
+      <div className="flex flex-col md:flex-row justify-between">
+        {/* Address Section */}
+        <div className="mb-5 md:mb-0">
+          <h1 className="text-[#1ABC9C]">
+            {language === "en" ? "ADDRESS" : "ADRESSE"}
+          </h1>
+          <div className="w-16 bg-blue-700 h-1 my-2" />
+          <div className="text-white text-lg">
+            {Locations.map((location, index) => (
+              <div key={index} className="flex items-center mb-2">
+                <location.location className="text-red-700 mr-2" />
+                <p>{`${location.country}: ${location.street}`}</p>
+              </div>
+            ))}
+          </div>
+        </div>
 
-.footer-links {
-  margin: 1rem 0;
-}
+        {/* Useful Links Section */}
+        <div className="mb-5 md:mb-0">
+          <h1 className="text-[#1ABC9C]">
+            {language === "en" ? "USEFUL LINKS" : "LIENS UTILES"}
+          </h1>
+          <div className="w-16 bg-blue-700 h-1 my-2" />
+          <div className="text-white text-lg">
+            {NavigationItemsRoutes.map((route, index) => (
+              <Link
+                href={route.path}
+                key={index}
+                className="block hover:text-blue-400 mb-1"
+              >
+                {route[language]}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        {/* Contacts Section */}
+        <div>
+          <h1 className="text-[#1ABC9C]">
+            {language === "en" ? "CONTACTS" : "CONTACTS"}
+          </h1>
+          <div className="w-16 bg-blue-700 h-1 my-2" />
+          <div className="text-white">
+            {Contacts.map((contact, index) => (
+              <div key={index} className="flex justify-between mb-2">
+                <p>{contact.country}</p>
+                <div className="flex items-center">
+                  <contact.icon className="text-blue-800 mr-2" />
+                  <p>{contact.phoneNumber}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex items-center text-white mt-4">
+            <FaEnvelope className="mr-2" />
+            <a href="mailto:info@cyberprogroup.com">info@cyberprogroup.com</a>
+          </div>
+        </div>
+      </div>
+
+      {/* Subscribe & Social Media */}
+      <div className="flex flex-col md:flex-row items-center justify-between mt-5">
+        <div className="flex items-center gap-3 mb-4 md:mb-0 w-full">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder={
+              language === "en"
+                ? "Please Enter Your Email"
+                : "Veuillez entrer votre email"
+            }
+            className="bg-transparent placeholder:text-[#F39C12] p-2 border rounded-l-lg focus:outline-none"
+          />
+          <button
+            onClick={handleSubscribe}
+            className={`py-2 px-4 rounded-r-lg ${
+              loading ? "bg-slate-400" : "bg-[#F39C12] hover:bg-[#ffe042]"
+            }`}
+          >
+            {language === "en" ? "SUBSCRIBE" : "S'ABONNER"}
+          </button>
+          <SiSpringsecurity className="w-7 h-7" />
+        </div>
+
+        <div className="bg-blue-700 p-3 rounded-lg">
+          <p className="text-white text-center">
+            {language === "en" ? "Follow Us" : "Suivez-nous"}
+          </p>
+          <div className="flex  gap-5 justify-around mt-2">
+            <a
+              href="https://www.tiktok.com/@cyberpro.group.rw?_t=8qR5bmcQDdG&_r=1"
+              className="text-white hover:animate-bounce"
+            >
+              <FaTiktok />
+            </a>
+            <a href="#" className="text-red-600 hover:animate-bounce">
+              <FaYoutube />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/cyberpro-group-rw-aa104932a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+              className="text-blue-500 hover:animate-bounce"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fcyberprogrouprw%2F%3Fnext%3D%252F&is_from_rle"
+              className="text-orange-500 hover:animate-bounce"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://x.com/CyberProGroupRw?t=q15PgUJVt6-ouzlMSiIa8Q&s=09&mx=2"
+              className="text-blue hover:animate-bounce"
+            >
+              <FaXTwitter />
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61565121267543"
+              className="text-blue-400 hover:animate-bounce"
+            >
+              <FaFacebookF />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer Links & Copyright */}
+      <div className="flex flex-col items-center mt-5">
+        <div className="flex justify-center gap-5 mb-4">
+          <Link href="/terms" className="text-white hover:text-gray-400">
+            {language === "en" ? "Website Terms" : "Conditions d'utilisation"}
+          </Link>
+          <Link href="/privacy" className="text-white hover:text-gray-400">
+            {language === "en" ? "Data Privacy" : "Confidentialité des données"}
+          </Link>
+          <Link href="/faq" className="text-white hover:text-gray-400">
+            FAQs
+          </Link>
+        </div>
+        <p className="text-white text-center">
+          {language === "en"
+            ? "copyright©2024 CyberProGroup"
+            : "droits d'auteur©2024 CyberProGroup"}
+        </p>
+        <p className="text-white text-center">
+          {language === "en" ? "All rights reserved" : "Tous droits réservés"}
+        </p>
+      </div>
+
+      <h1 className="text-teal-400 font-bold text-lg text-center mt-4">
+        Designed and Developed by{" "}
+        <a
+          className="text-[#34b886] hover:text-[#2e7451]"
+          href="https://www.edgereachtech.com"
+        >
+          EdgeReach Tech
+        </a>
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
